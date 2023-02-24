bb81597590f9a08328d198a40cf6e0ce8f2ede7c
James Hiew <james@hiew.net>
2023-02-24 13:37:19 +0000

    Let HTTP_HOST and HTTP_PORT be configurable

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 use axum::{response::Html, routing::get, Router};
-use std::net::SocketAddr;
+use eyre::Result;
+use std::env;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
     let app = Router::new().route("/", get(index));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let host = env::var("HTTP_HOST").unwrap_or("127.0.0.1".into());
+    let port = env::var("HTTP_PORT").unwrap_or("3000".into());
+    let addr = format!("{host}:{port}").parse()?;
+
     tracing::info!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+
+    Ok(())
 }
 
 async fn index() -> Html<&'static str> {

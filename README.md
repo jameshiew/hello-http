e65c9352e33bbd3277bbe212bac99b872da1e869
James Hiew <james@hiew.net>
2023-02-25 14:48:37 +0000

    Add updateable readyz and livez endpoints

--- README.md
+++ README.md
@@ -8,3 +8,17 @@ This is a simple HTTP server packaged inside a Docker image, for testing/demonst
 | -------------------- | ----------------------------------------------- | ------------- |
 | `HTTP_HOST`          | The IP address to which the server should bind. | `127.0.0.1`   |
 | `HTTP_PORT`          | The port number to listen on.                   | `3000`        |
+
+## Endpoints
+
+`GET /` - Hello world HTML index page
+`GET /readyz` - 200 if ready, 503 if not
+`GET /livez` - 200 if live, 500 if not
+`POST /` - post a JSON like below to update the readiness/liveness of the service
+
+```json
+{
+  "ready": false,
+  "live": false
+}
+```
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
 use axum::{
+    extract::State,
     http::{Request, Response, StatusCode},
     response::Html,
     routing::get,
-    Router,
+    Json, Router,
 };
 use eyre::Result;
-use std::{env, time::Duration};
+use serde::{Deserialize, Serialize};
+use std::{
+    env,
+    sync::{Arc, RwLock},
+    time::Duration,
+};
 use tower_http::trace::TraceLayer;
 use tracing::Span;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct App {
+    ready: bool,
+    live: bool,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            ready: true,
+            live: true,
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
+    let shared_state = Arc::new(RwLock::new(App::default()));
 
-    let app = Router::new()
-        .route("/", get(index))
+    let router = Router::new()
+        .route("/", get(index).post(update))
         .route("/readyz", get(readyz))
         .route("/livez", get(livez))
         .layer(
@@ -27,7 +49,8 @@ async fn main() -> Result<()> {
                     let status = response.status();
                     tracing::info!(?latency, ?status, "Sent response");
                 }),
-        );
+        )
+        .with_state(shared_state);
 
     let host = env::var("HTTP_HOST").unwrap_or("127.0.0.1".into());
     let port = env::var("HTTP_PORT").unwrap_or("3000".into());
@@ -35,7 +58,7 @@ async fn main() -> Result<()> {
 
     tracing::info!("listening on {}", addr);
     axum::Server::bind(&addr)
-        .serve(app.into_make_service())
+        .serve(router.into_make_service())
         .await
         .unwrap();
 
@@ -46,10 +69,28 @@ async fn index() -> Html<&'static str> {
     Html("<p>Hello, World!</p>")
 }
 
-async fn livez() -> StatusCode {
+async fn update(app: State<Arc<RwLock<App>>>, Json(new): Json<App>) -> StatusCode {
+    let mut app = app.as_ref().write().unwrap();
+    let App { ready, live } = new;
+    app.ready = ready;
+    app.live = live;
     StatusCode::OK
 }
 
-async fn readyz() -> StatusCode {
-    StatusCode::OK
+async fn livez(State(state): State<Arc<RwLock<App>>>) -> StatusCode {
+    let live = state.read().unwrap().live;
+    if live {
+        StatusCode::OK
+    } else {
+        StatusCode::INTERNAL_SERVER_ERROR
+    }
+}
+
+async fn readyz(State(state): State<Arc<RwLock<App>>>) -> StatusCode {
+    let ready = state.read().unwrap().ready;
+    if ready {
+        StatusCode::OK
+    } else {
+        StatusCode::SERVICE_UNAVAILABLE
+    }
 }

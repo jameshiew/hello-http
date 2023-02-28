1a514fc3099c0507e4b5deb87ed229bc7eb50a66
James Hiew <james@hiew.net>
2023-02-28 12:32:31 +0000

    Handle quit signals e.g. Ctrl+C

--- src/main.rs
+++ src/main.rs
@@ -93,6 +93,7 @@ async fn main() -> Result<()> {
     tracing::info!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(router.into_make_service())
+        .with_graceful_shutdown(shutdown_handler())
         .await
         .unwrap();
 
@@ -127,4 +128,24 @@ async fn readyz(State(state): State<Arc<RwLock<App>>>) -> StatusCode {
     } else {
         StatusCode::SERVICE_UNAVAILABLE
     }
+}
+
+async fn shutdown_handler() {
+    let mut interrupts = tokio::signal::unix::signal(tokio::signal::unix::SignalKind::interrupt())
+        .expect("failed to register SIGINT handler");
+    let mut terminates = tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+        .expect("failed to register SIGTERM handler");
+    let mut quits = tokio::signal::unix::signal(tokio::signal::unix::SignalKind::quit())
+        .expect("failed to register SIGQUIT handler");
+    tokio::select! {
+        _ = interrupts.recv() => {
+            tracing::info!("received SIGINT, shutting down");
+        }
+        _ = terminates.recv() => {
+            tracing::info!("received SIGTERM, shutting down");
+        }
+        _ = quits.recv() => {
+            tracing::info!("received SIGQUIT, shutting down");
+        }
+    };
 }

f42bb5f72240c0c54afe51c0995587c5ccd4afbf
James Hiew <james@hiew.net>
2025-06-04 11:20:56 +0100

    Extract defaults to constants

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,13 @@
 use std::env;
 
+pub const DEFAULT_HOST: &str = "127.0.0.1";
+pub const DEFAULT_PORT: &str = "3000";
+
 pub const READYZ_ROUTE: &str = "/readyz";
 pub const LIVEZ_ROUTE: &str = "/livez";
 
 pub fn get_server_addr() -> String {
-    let host = env::var("HTTP_HOST").unwrap_or("127.0.0.1".into());
-    let port = env::var("HTTP_PORT").unwrap_or("3000".into());
+    let host = env::var("HTTP_HOST").unwrap_or_else(|_| DEFAULT_HOST.into());
+    let port = env::var("HTTP_PORT").unwrap_or_else(|_| DEFAULT_PORT.into());
     format!("{host}:{port}")
 }
--- src/logging.rs
+++ src/logging.rs
@@ -3,8 +3,11 @@ use std::env;
 use tracing::metadata::LevelFilter;
 use tracing_subscriber::{EnvFilter, fmt};
 
+pub const DEFAULT_LOG_ANSI: &str = "1";
+pub const DEFAULT_LOG_LEVEL: LevelFilter = LevelFilter::INFO;
+
 pub fn init_logger() {
-    let use_ansi = env::var("HTTP_LOG_ANSI").unwrap_or("1".to_owned());
+    let use_ansi = env::var("HTTP_LOG_ANSI").unwrap_or_else(|_| DEFAULT_LOG_ANSI.to_owned());
     let use_ansi = !(use_ansi.is_empty() || use_ansi == "0"); // i.e. HTTP_LOG_ANSI=0 turns it off
 
     let format = fmt::format()
@@ -15,7 +18,7 @@ pub fn init_logger() {
     tracing_subscriber::fmt()
         .with_env_filter(
             EnvFilter::builder()
-                .with_default_directive(LevelFilter::INFO.into())
+                .with_default_directive(DEFAULT_LOG_LEVEL.into())
                 .from_env_lossy(),
         )
         .event_format(format)
